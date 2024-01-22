2c202cbb5fb637d4004b82601d834d23a9e92170
code guru <guru@macbook-air.lan>
2024-01-21 18:38:53 -0800

    Added util bar

--- client/src/components/modules/UtilBar.tsx
+++ client/src/components/modules/UtilBar.tsx
@@ -1,8 +1,26 @@
 import React from "react";
+import "./UtilBar.css";
 
 type Props = {};
 const UtilBar = (props: Props) => {
-  return <>Util Bar says: Hello world!</>;
+  const makeFiltersDropdown = () => {
+    console.log("Clicked Filters Button");
+  };
+  return (
+    <>
+      <span>
+        <button className="UtilBarButton" onClick={makeFiltersDropdown}>
+          Filters
+        </button>
+      </span>
+      <span>
+        <input className="SearchBar" defaultValue="Search" />
+      </span>
+      <span>
+        <button className="UtilBarButton AddButton">+</button>
+      </span>
+    </>
+  );
 };
 
 export default UtilBar;
