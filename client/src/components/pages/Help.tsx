7acf8f1254e677455d6f4cfa49dca346340391ae
code guru <guru@macbook-air.lan>
2024-01-30 20:33:34 -0800

    Minor changes to help page

--- client/src/components/pages/Help.tsx
+++ client/src/components/pages/Help.tsx
@@ -10,22 +10,21 @@ const Help = (props: Props) => {
       <h1 className="u-textCenter">What is this site?</h1>
       <div className="helpText">
         <p>
-          This is a site where you can store all your favorites for future
-          reference. The hope is that, when you're feeling a bit sad, you might
-          pull up this site and feel a bit cheered. At the top of your homepage,
-          we've highlighted a random favorite for you to enjoy.
+          This is a site where you can store all your favorites for future reference. The hope is
+          that, when you're feeling a bit sad, you might pull up this site and feel a bit cheered.
+          At the top of your homepage, we've highlighted a random favorite for you to enjoy.
         </p>
         <p>
-          If you click open your profile page, note that there are such things
-          as "friends." To add friends, go to the Community page (or click on
-          the + button in your profile), search for your friend's name, click on
-          their profile, and send them a friend request. After you become
-          friends, you can see each others' public favorites!
+          If you click open your profile page, note that there are such things as "friends." To add
+          friends, go to the Community page (or click on the + button in your profile), search for
+          your friend's name, click on their profile, and send them a friend request. After you
+          become friends, you can see each others' public favorites!
         </p>
         <p>Give our site a go. We hope you like it!</p>
         <p>
-          Some caveats: Tags cannot have commas in them, and cannot be the empty
-          string.
+          Some caveats: Tags cannot have commas in them, and cannot be the empty string. You won't
+          show up in the community page until you make a profile, although you can still send friend
+          requests. Note that your profile will not auto-fill with your google information.
         </p>
       </div>
     </div>
